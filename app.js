6abb3853985cebf814ba4f17729b9a9a0699a0e9
Alexandre Riquet <alexandre.riquet@gmail.com>
2021-03-01 16:56:11 +0100

    Submit configuration form

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 class PixelArtCanvas {
   constructor(config) {
     this.container = document.getElementById('canvas');
-    this.gridLineSize = 16;
-    this.pixelSize = 30;
+    this.gridLineDefaultSize = 8;
+    this.pixelDefaultSize = 30;
+    this.gridLineSize = this.gridLineDefaultSize;
+    this.pixelSize = this.pixelDefaultSize;
     this.pixelColorBase = 1;
   }
 
@@ -10,7 +12,7 @@ class PixelArtCanvas {
     // Pixels grid generation
     this.generateGrid();
     
-    // Grid dimensions personnalisation 
+    // Grid size configuration 
     this.generateConfigForm();
 
     // Color change on pixels click
@@ -28,6 +30,9 @@ class PixelArtCanvas {
   generateGrid() {
     const gridSize = this.gridLineSize*this.gridLineSize;
 
+    // Grid reset
+    this.container.innerHTML = '';
+
     // Add width dimension to contains good pixels number by line
     this.container.style.width = (this.pixelSize*this.gridLineSize)+'px';
 
@@ -44,22 +49,35 @@ class PixelArtCanvas {
 
   }
 
+  /**
+   * Configuration user form
+   */
   generateConfigForm() {
     const form = document.createElement('form');
     const gridLineSizeForm = document.createElement('input');
     const pixelSizeForm = document.createElement('input');
     const button = document.createElement('button');
     
+    // Form HTML params
     form.className = 'configuration';
     
+    // Input Grid line size HTML params
     gridLineSizeForm.type = 'number';
+    gridLineSizeForm.name = 'grid-line-size';
+    gridLineSizeForm.min = 1;
+    gridLineSizeForm.max = 30;
     gridLineSizeForm.className = 'configuration_input';
     gridLineSizeForm.placeholder = 'Grid size (8 by default)';
 
+    // Input pixel size HTML params
     pixelSizeForm.type = 'number';
+    pixelSizeForm.name = 'pixel-size';
+    pixelSizeForm.min = 10;
+    pixelSizeForm.max = 90;
     pixelSizeForm.className = 'configuration_input';
     pixelSizeForm.placeholder = 'Pixels size (30 by default)';
     
+    // Submit button HTML params
     button.type = 'submit';
     button.className = 'configuration_button';
     button.textContent = 'Generate';
@@ -67,10 +85,21 @@ class PixelArtCanvas {
     form.append(gridLineSizeForm);
     form.append(pixelSizeForm);
     form.append(button);
-    
-    
+
+    // Attach submit event
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      // If no values in input => set defaultValues
+      this.pixelSize = pixelSizeForm.value ?  pixelSizeForm.value : this.pixelDefaultSize;
+      this.gridLineSize = gridLineSizeForm.value ? gridLineSizeForm.value : this.gridLineDefaultSize;
+
+      this.generateGrid();
+    });
+
     document.body.prepend(form);
   }
+  
 }
 
 const myPixelArt = new PixelArtCanvas();
